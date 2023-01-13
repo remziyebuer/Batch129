f76e40ad762eb5ca48a024e10333558f40ab48e4
remziyebuer <115974069+remziyebuer@users.noreply.github.com>
2023-01-13 22:55:59 +0100

    Update GaniRegex.java

--- src/main/java/advanced_java/practice1/GaniRegex.java
+++ src/main/java/advanced_java/practice1/GaniRegex.java
@@ -6,21 +6,21 @@ public class GaniRegex {
     public static void main(String[] args) {
 
         Scanner input= new Scanner(System.in);
-        System.out.println("Bosluk birakarak adyoad");
+        System.out.println("Geben SIe bitte eine Vorname-Nachname");
         String adsoyad= input.nextLine().replaceAll("[^a-zA-Z]"," ")
                 .replaceAll("\\s+"," ")
                 .trim();
-        System.out.println("adsoyad = " + adsoyad);
-        String ad= adsoyad.split(" ")[0];
-        String soyad=adsoyad.split(" ")[1];
+        System.out.println("VorNachname = " + adsoyad);
+        String vorname= adsoyad.split(" ")[0];
+        String nachname=adsoyad.split(" ")[1];
         String adilkharf= ad.substring(0,1).toUpperCase();
         String soyadilkharf=soyad.substring(0,1).toUpperCase();
-        System.out.println("adilkharf = " + adilkharf);
-        System.out.println("soyadilkharf = " + soyadilkharf);
+        System.out.println("ersteBuchstabenVorname = " + adilkharf);
+        System.out.println("ersteBuchstabennachname = " + soyadilkharf);
         String formatliad=adilkharf+ad.substring(1).toLowerCase();
         String formatlisoyad=soyadilkharf+soyad.substring(1).toLowerCase();
-        System.out.println("formatliad = " + formatliad);
-        System.out.println("formatlisoyad = " + formatlisoyad);
+        System.out.println("formatliVorname = " + formatliad);
+        System.out.println("formatliNachname = " + formatlisoyad);
 
     }
 }
