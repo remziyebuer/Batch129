02bd762202070d8aeb089de405976b9eda40a960
remziyebuer <115974069+remziyebuer@users.noreply.github.com>
2023-01-19 22:48:01 +0100

    ...

---
+++ src/main/java/java_core/day28_exceptions/Exceptions04.java
@@ -0,0 +1,26 @@
+package java_core.day28_exceptions;
+
+public class Exceptions04 {
+    public static void main(String[] args) {
+
+        String arr[] = {"A", "G", "M", "L"};
+
+        getElementFromArray(arr, 1);
+        getElementFromArray(arr, 5);
+        System.out.println("Hi!");
+
+    }
+
+    //Array'lerde olmayan bir index kullanildiginda Java ArrayIndexOutOfBoundsException atar.
+    public static void getElementFromArray(String[] s, int idx){
+
+        try {
+            String el = s[idx];
+            System.out.println(el);
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+            System.out.println(e.getCause());
+        }
+
+    }
+}
