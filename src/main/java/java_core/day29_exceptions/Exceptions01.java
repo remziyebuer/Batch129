300a5760732d0077d579a51428b0209169de32c6
remziyebuer <115974069+remziyebuer@users.noreply.github.com>
2023-01-20 23:57:12 +0100

    ...

---
+++ src/main/java/java_core/day29_exceptions/Exceptions01.java
@@ -0,0 +1,25 @@
+package java_core.day29_exceptions;
+
+public class Exceptions01 {
+    public static void main(String[] args) {
+
+        Object obj = 70;
+        String str = "";
+
+        try {
+            str = (String) obj;// Birbirine donusturulemeyecek data type'larini donusturmekte israr ederseniz Java ClassCastException atar
+            System.out.println(str);
+        } catch (ClassCastException e) {
+            System.out.println("Her data type'i her data type'ine cevrilmez");
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
