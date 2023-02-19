d70ab47a13e756707abe59f652dda81a0d3918ad
ÂRemziye Buer <115974069+remziyebuer@users.noreply.github.com>
2023-02-19 22:50:07 +0100

    ...

---
+++ src/main/java/miniproject/okulb/öz/Runner.java
@@ -0,0 +1,9 @@
+package miniproject.okulb.öz;
+
+public class Runner {
+    public static void main(String[] args) throws InterruptedException {
+        OgretmenDepo.feykOgretmenEkle();
+        OkulDepo.anaMenu();
+
+    }
+}
