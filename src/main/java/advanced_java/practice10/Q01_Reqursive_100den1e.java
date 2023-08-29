de3e7d2444abfa30e915db9590c7e8c103e4e658
isminiz <115974069+remziyebuer@users.noreply.github.com>
2023-08-29 23:55:35 +0200

    ...

---
+++ src/main/java/advanced_java/practice10/Q01_Reqursive_100den1e.java
@@ -0,0 +1,20 @@
+package advanced_java.practice10;
+
+public class Q01_Reqursive_100den1e {
+    //100'den 1'e kadar sayıları loop ve sayı kullanmadan yazdıran bir method oluşturunuz.
+    public static void main(String[] args) {
+
+        sayilariYazdir('d');
+
+    }
+
+    public static void sayilariYazdir(char ch) {
+
+        if (ch >= 'a' / 'a') {
+            System.out.print((int) ch + " ");
+            ch--;
+            sayilariYazdir(ch);
+        }
+    }
+
+}
